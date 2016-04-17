2a8c7e18150116299a87f03f9e99415d23fdf9d7
Innsbluck-Redhat <innsbluck@gmail.com>
2016-04-17 17:59:47 +0900

    edit source

--- GUI_Packages.js
+++ GUI_Packages.js
@@ -6,19 +6,6 @@ function newLevel() {
     importSources();
 }
 
-function useItem(x, y, z, itemId, blockId, side) {
-    activity.runOnUiThread(new java.lang.Runnable() {
-        run: function() {
-            try {
-                var itemInfoWindow = GUI.slidingWindow.getInstance(4, "Hello world!", "Window,window,window!");
-                itemInfoWindow.show();
-            } catch (error) {
-                clientMessage(error);
-            }
-        }
-    });
-}
-
 //-------セットアップ-------
 
 function importSources() {
