36e9818052dde9a29c72536cce99a07c8a432b49
Innsbluck-Redhat <innsbluck@gmail.com>
2016-04-17 20:56:46 +0900

    edit readme

--- Samples/SlidingWindow.js
+++ Samples/SlidingWindow.js
@@ -10,8 +10,8 @@ function useItem(x, y, z, itemId, blockId, side) {
     activity.runOnUiThread(new java.lang.Runnable() {
         run: function() {
             try {
-                var itemInfoWindow = GUI.slidingWindow.getInstance(4, "Hello world!", "Window,window,window!");
-                itemInfoWindow.show();
+                var infoWindow = GUI.slidingWindow.getInstance(4, "Hello world!", "Window,window,window!");
+                infoWindow.show();
             } catch (error) {
                 clientMessage(error);
             }
